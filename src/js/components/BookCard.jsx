407fb28ebe5ac21fb1beed95558f00d3f07f1772
Shane B <dotfortun@users.noreply.github.com>
2025-12-05 23:45:41 +0000

    Demonstrated hooks.

--- src/js/components/BookCard.jsx
+++ src/js/components/BookCard.jsx
@@ -1,52 +1,52 @@
 const BookCard = ({ book }) => {
   return (
-    <div class="card mx-auto mb-2">
+    <div className="card mx-auto mb-2">
       <img
         src={book.cover ? book.cover : "https://placehold.co/300x500/888/888/"}
-        class="card-img-top"
+        className="card-img-top"
         alt="..."
       />
-      <div class="card-body">
-        <h5 class="card-title">
+      <div className="card-body">
+        <h5 className="card-title">
           {book.title}
           {book.year_published ? ` — ${book.year_published}` : ""}
         </h5>
-        <ul class="list-group list-group-flush my-2">
+        <ul className="list-group list-group-flush my-2">
           {book.author ? (
-            <li class="list-group-item">By: {book.author}</li>
+            <li className="list-group-item">By: {book.author}</li>
           ) : (
             ""
           )}
           {book.num_pages ? (
-            <li class="list-group-item">{book.num_pages} pgs.</li>
+            <li className="list-group-item">{book.num_pages} pgs.</li>
           ) : (
             ""
           )}
           {book.year_published ? (
-            <li class="list-group-item">Published {book.year_published}</li>
+            <li className="list-group-item">Published {book.year_published}</li>
           ) : (
             ""
           )}
           {book.isbn10 ? (
-            <li class="list-group-item">ISBN-10: {book.isbn10}</li>
+            <li className="list-group-item">ISBN-10: {book.isbn10}</li>
           ) : (
             ""
           )}
           {book.isbn13 ? (
-            <li class="list-group-item">ISBN-13: {book.isbn13}</li>
+            <li className="list-group-item">ISBN-13: {book.isbn13}</li>
           ) : (
             ""
           )}
         </ul>
       </div>
-      <div class="card-footer d-flex justify-content-between mt-auto mb-2">
-        <button class="btn btn-info bg-gradient text-dark">
-          <i class="fa-solid fa-thumbs-down"></i>
-          <i class="fa-solid fa-book-skull"></i>
+      <div className="card-footer d-flex justify-content-between mt-auto mb-2">
+        <button className="btn btn-info bg-gradient text-dark">
+          <i className="fa-solid fa-thumbs-down"></i>
+          <i className="fa-solid fa-book-skull"></i>
         </button>
-        <button class="btn btn-danger bg-gradient text-dark">
-          <i class="fa-solid fa-thumbs-up"></i>
-          <i class="fa-solid fa-fire-burner"></i>
+        <button className="btn btn-danger bg-gradient text-dark">
+          <i className="fa-solid fa-thumbs-up"></i>
+          <i className="fa-solid fa-fire-burner"></i>
         </button>
       </div>
     </div>
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import BookCard from "./BookCard";
 import { Col, Container, Row } from "./Layout";
@@ -7,21 +7,62 @@ import json_data from "../data";
 
 //create your first component
 const Home = () => {
+  const [books, setBooks] = useState([]);
   const [bookIdx, setBookIdx] = useState(
-    Math.floor(Math.random() * json_data.books.length)
+    Math.floor(Math.random() * books.length)
   );
 
-  setInterval(() => console.log("This should be once a second, right?"), 1000);
+  const [input, setInput] = useState("");
+
+  // Using useEffect to give side effects to you data.
+  // useEffect(() => {
+  //   console.log("This will run whenever input changes now!");
+  //   console.log(input);
+  // }, [input]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const resp = await fetch("https://library.dotlag.space/library");
+
+      if (resp.ok) {
+        const data = await resp.json();
+        setBooks(data.books);
+      }
+    };
+
+    getData();
+  }, []);
+
+  // How to deal with the double counting intervals:
+  // useEffect(() => {
+  //   const interval = setInterval(
+  //     () => console.log("This should be once a second, right?"),
+  //     1000
+  //   );
+
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <>
+      {/* <Container isFluid>
+        <Row className="my-r">
+          <Col>
+            <input
+              type="text"
+              value={input}
+              onChange={(ev) => setInput(ev.target.value)}
+            />
+          </Col>
+        </Row>
+      </Container> */}
       <Container isFluid>
         <Row className="my-3">
           <Col className="d-flex justify-content-around">
             <button
               className="btn btn-primary"
               onClick={() => {
-                setBookIdx(Math.floor(Math.random() * json_data.books.length));
+                setBookIdx(Math.floor(Math.random() * books.length));
               }}
             >
               <h2>Pick a random book to read:</h2>
@@ -30,7 +71,7 @@ const Home = () => {
         </Row>
         <Row>
           <Col colWidth={4} colOffset={4} className="d-flex flex-column gap-3">
-            <BookCard book={json_data.books[bookIdx]} />
+            {books.length ? <BookCard book={books[bookIdx]} /> : <></>}
           </Col>
         </Row>
       </Container>
